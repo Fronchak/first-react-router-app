ca0b7d0e34a80475d861f1d96d79aaf6bd80f5c4
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-09-26 23:01:40 -0300

    Concluído primeiro projeto

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import Root, {
 import ErrorPage from './error-page';
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from './routes/contact';
 import EditContact, {
   action as editAction,
@@ -29,24 +30,30 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        index: true,
-        element: <Index />,
-      },
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>
+          }
+        ],
       }
     ]
   },
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,8 +1,23 @@
-import { Form, useLoaderData } from 'react-router-dom';
-import { getContact } from '../contacts';
+import { Form, useLoaderData, useFetcher } from 'react-router-dom';
+import { getContact, updateContact } from '../contacts';
 
 export async function loader({ params }) {
-  return getContact(params.contactId);
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: 'Not Found'
+    });
+  }
+  return contact
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updates = {
+    favorite: formData.get('favorite') === "true"
+  };
+  await updateContact(params.contactId, updates);
 }
 
 export default function Contact() {
@@ -68,9 +83,15 @@ export default function Contact() {
 }
 
 function Favorite ({ contact }) {
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
+
+  if(fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -82,6 +103,6 @@ function Favorite ({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
